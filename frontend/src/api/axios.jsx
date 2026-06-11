c6a00d3228f723ca928d4a258ee7adbd8c6f237e
elvis-le <khanh321zx@gmail.com>
2026-06-11 11:56:39 +0700

    UPDATE LINK

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "manage-wo-production.up.railway.app/api",
+  baseURL: "https://manage-wo-production.up.railway.app/api",
 });
 
 export default api;
